e2cd84c67d6f0f4b5b12b234eda0337294f73f4a
Francisco Oliveira <delley.fx@gmail.com>
2017-04-23 00:08:11 -0300

    Initial commit

---
+++ src/main/java/br/com/froli/miniblog/connection/ServiceLocator.java
@@ -0,0 +1,41 @@
+package br.com.froli.miniblog.connection;
+
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.client.MongoDatabase;
+
+public class ServiceLocator {
+
+	private static final String HOST = "192.168.0.65";
+	private static final String PORT = "27017";
+	private static final String DB_NAME = "blog";
+	private static final String URI = String.format("mongodb://%s:%s", HOST, PORT);
+
+	private static ServiceLocator instance;
+
+	private MongoDatabase database;
+
+	private ServiceLocator() {
+	}
+
+	public static ServiceLocator getInstance() {
+		if (instance == null) {
+			instance = new ServiceLocator();
+		}
+		return instance;
+	}
+
+	public MongoDatabase getDatabase() {
+
+		if (database == null) {
+			try (MongoClient mongoClient = new MongoClient(new MongoClientURI(URI))) {
+				database = mongoClient.getDatabase(DB_NAME);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+
+		return database;
+	}
+
+}
